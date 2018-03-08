0072ac60f65e761e2f190e1220df3f759fbeee89
jcarpio <jose.carpio@gmail.com>
2018-03-08 10:03:16 +0100

    creado resta peano

--- suma_peano.pl
+++ suma_peano.pl
@@ -3,3 +3,8 @@
 % de X en Y en aritmética de Peano
 suma(0, Y, Y).
 suma(s(X), Y, s(Z)):- suma(X, Y, Z).
+
+% resta(?X, ?Y, ?Z)
+% Es cierto cuando Z unifica con la resta de 
+% X menos Y en aritmética de Peano
+resta(X,Y,Z) :- suma(Y,Z,X).
