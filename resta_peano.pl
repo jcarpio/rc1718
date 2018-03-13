a6f03bd95f4787a790864fd97c64183aa0329bf3
jcarpio <jose.carpio@gmail.com>
2018-03-13 10:52:15 +0100

    aniadida resta_peano

---
+++ resta_peano.pl
@@ -0,0 +1,11 @@
+% resta(?X, ?Y, ?Z)
+% Es cierto cuando Z unifica con 
+% la resta de X menos Y en 
+% Aritmética de Peano. X tine que
+% ser mayor que Y.
+
+% Principio de Inducción
+% P(no)
+% P(n-1) -> P(n)
+resta(X, 0, X).
+resta(X, s(Y), s(Z)):- resta(X, Y, Z).
