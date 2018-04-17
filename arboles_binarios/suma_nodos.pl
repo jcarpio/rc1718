95ac2ca78fe2d81a19dc09b2c0e435f5653fc1f3
Your Name <you@example.com>
2018-04-17 12:27:26 +0200

    simetrico, invertir, simetricos

--- arboles_binarios/suma_nodos.pl
+++ arboles_binarios/suma_nodos.pl
@@ -1,4 +1,14 @@
 
 % suma_nodos(+A, -R)
 % Es cierto cuando R unifica con la suma de
-% todos las etiquetas de árbol A. 
+% todos las etiquetas de árbol A.
+
+suma_nodos(nil, 0).
+suma_nodos(a(E, HI, HD), R):-
+ suma_nodos(HI, RI),
+ suma_nodos(HD, RD),
+ R is E + RI + RD.	
+
+arbol2( 
+       a(1, a(2,a(3,nil,nil),nil), a(4,nil,nil))
+     ). 
