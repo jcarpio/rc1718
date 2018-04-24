22250c79d190e85f1948177cc6c0e4f7b88a1674
Your Name <you@example.com>
2018-04-24 12:30:10 +0200

    redocorridos -> anchura

--- arboles_binarios/recorridos.pl
+++ arboles_binarios/recorridos.pl
@@ -31,16 +31,41 @@ postorden(a(E, HI, HD), R):-
   postorden(HI, RI),
   postorden(HD, RD),
   append([RI, RD, [E]], R).
+% bin2gen(+A, -R)
+% Es cierto cuando R unifica con un árbol
+% genérico equivalente al arbol binario A
+
+bin2gen(a(E, HI, HD), a(E, [RI, RD])):-
+  HI \= nil, HD \= nil,
+  bin2gen(HI, RI),
+  bin2gen(HD, RD).
+
+bin2gen(a(E, HI, nil), a(E, [RI])):-
+  bin2gen(HI, RI).
+
+bin2gen(a(E, nil, HD), a(E, [RD])):-
+  bin2gen(HD, RD).
+
+bin2gen(a(E, nil, nil), a(E, [])).
+
 
 % anchura(+A, -R)
 % Es cierto cuando R unifica con el recorrido
 % en anchura del árbol A
 
-anchura(nil, []).
-% anchura(a(E, HI, HD),  ):-
-%   anchura(
+anchura(a(E, Lista_hijos), [E|R]):-
+  anchural(Lista_hijos, R).
 
+anchural([], []).
+anchural([a(E, L)|Resto], [E|R2]):-  
+  append(Resto, L, R),
+  anchural(R, R2).
+   
 
 arbol1(  
-a('+', a(1,nil,nil), a('*', a(2,nil,nil), a(3,nil,nil)))
-).
+ a('+', a(1,nil,nil), a('*', a(2,nil,nil), a(3,nil,nil)))
+ ).
+
+arbol2(  
+ a('+', a('/', a(5, nil,nil), a(6,nil,nil)), a('*', a(2,nil,nil), a(3,nil,nil)))
+ ).
