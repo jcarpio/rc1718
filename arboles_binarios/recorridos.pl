75edee1ceb27bf3e7af225e6502ca3fdd653b6f2
Your Name <you@example.com>
2018-04-24 11:08:41 +0200

    recorridos

---
+++ arboles_binarios/recorridos.pl
@@ -0,0 +1,39 @@
+% inorden(+A, -R)
+% Es cierto cuando R unifica con el recorrido
+% en inorden del árbol A
+% Hi, Raiz, Hd
+inorden(nil, []).
+inorden(a(E, HI, HD), R):-
+  inorden(HI, RI),
+  inorden(HD, RD),
+  append(RI, [E|RD], R).
+
+% preorden(+A, -R)
+% Es cierto cuando R unifica con el recorrido
+% en preorden del árbol A
+% Raiz, Hi, Hd
+
+preorden(nil, []).
+preorden(a(E, HI, HD), R):-
+  preorden(HI, RI),
+  preorden(HD, RD),
+  append([E|RI], RD, R).
+
+% postorden(+A, -R)
+% Es cierto cuando R unifica con el recorrido
+% en postorden del árbol A
+% Hi, Hd, Raiz
+
+postorden(nil, []).
+postorden(a(E, HI, HD), R):-
+  postorden(HI, RI),
+  postorden(HD, RD),
+  append([RI, RD, [E]], R).
+
+
+arbol1(  
+a('+', a(1,nil,nil), a('*', a(2,nil,nil), a(3,nil,nil)))
+).
+
+
+).
