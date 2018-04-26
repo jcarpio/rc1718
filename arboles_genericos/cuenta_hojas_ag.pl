fef91b943555fe08f8bdfc622e31b5df083100e8
Your Name <you@example.com>
2018-04-26 10:09:18 +0200

    cuenta_hojas.pl

---
+++ arboles_genericos/cuenta_hojas_ag.pl
@@ -0,0 +1,19 @@
+:- include('crea_arbolg.pl').
+:- include('ejemplos_ag.pl').
+
+% cuenta_hojas_ag(+A, -N)
+% Es cierto cuando N unifica con el número de hojas
+% del árbol genérico A
+
+cuenta_hojas_ag(a(_,Lista_hijos), R):-
+	Lista_hijos \= [],
+	cuenta_hojas_ag(Lista_hijos, R).
+
+cuenta_hojas_ag(a(_,[]), 1).
+
+cuenta_hojas_ag([],0).
+
+cuenta_hojas_ag([Cab|Resto], R):-
+	cuenta_hojas_ag(Cab, RCab),
+	cuenta_hojas(Resto, RResto),
+	R is RCab + RResto.
